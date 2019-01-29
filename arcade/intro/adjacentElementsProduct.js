aed74d73bafac7f334c7dcecae72190ee26e348b
Thomas Davis <41704687+ThomasMDavis@users.noreply.github.com>
2019-01-28 17:34:08 -0800

    Added let for loop index

--- arcade/intro/adjacentElementsProduct.js
+++ arcade/intro/adjacentElementsProduct.js
@@ -1,7 +1,7 @@
 function adjacentElementsProduct(inputArray) {
     let largestProduct = inputArray[0] * inputArray[1];
     
-    for (i = 1; i < inputArray.length - 1; i++) {
+    for (let i = 1; i < inputArray.length - 1; i++) {
         largestProduct = Math.max(largestProduct, inputArray[i] * inputArray[i + 1]);
     }
     
